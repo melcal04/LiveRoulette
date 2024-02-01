dc22e2cfa2a15580de3e84d657ffc802d7271379
Melchor B. Callos <mel.cal@leekie.com>
2024-02-01 11:20:52 +0800

    Update: Latest

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -28,7 +28,7 @@ import org.junit.runner.RunWith;
                 tags = "@Payout or @BetInformation or @Gameplay or @Functionality or @ResultAndStatistics or @UserInterface"
 
                 // ! Re Run
-                //tags = "@FunctionalityTest21"
+                //tags = "@GameplayTest2 or @GameplayTest23"
 
                 // ! Maintenance
                 //tags = "@Maintenance"
